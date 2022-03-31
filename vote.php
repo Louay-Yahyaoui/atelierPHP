3fb0ee45c40dc86a8ebb48708076621a27e7f021
Louay Yahyaoui <75395988+Louay-Yahyaoui@users.noreply.github.com>
2022-03-31 16:16:32 +0100

    Update vote.php

--- vote.php
+++ vote.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_COOKIE["cookie"]))
 {
-    die("Vous avez déjà voté");
+    die("Vous avez déjà voté.Vos réponses sont ".$_COOKIE['cookie']);
 }
-setcookie("cookie","",time()+120);
+setcookie("cookie",$_POST["question1"]." ".$_POST["question2"]." ".$_POST["question3"],time()+120);
 echo("Félicitations!Vous avez voté");
